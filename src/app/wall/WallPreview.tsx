b2ba25e72427c444e86cb38c16721d3d0b056ae9
shubham tyagi <tyagi.shubham177@gmail.com>
2025-09-28 11:59:54 +0530

    Fix wall page dynamic import for Next server rules

---
+++ src/app/wall/WallPreview.tsx
@@ -0,0 +1,18 @@
+﻿"use client";
+
+import dynamic from "next/dynamic";
+
+const DynamicWallScene = dynamic(() => import("@/components/wall/WallScene"), {
+  ssr: false,
+  loading: () => (
+    <div className="flex h-full items-center justify-center text-sm text-slate-400">
+      Loading wall preview...
+    </div>
+  ),
+});
+
+const WallPreview = () => {
+  return <DynamicWallScene />;
+};
+
+export default WallPreview;
